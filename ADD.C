e79e665b81b19b0ae545d8c635510aa47ad04a9a
VelVM11 <93313839+VelVM11@users.noreply.github.com>
2021-10-30 08:47:21 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b,c;
+   clrscr();
+   printf("enter the value of a :",a);
+   scanf("%d",&a);
+   printf("enter the value of b ",b);
+   scanf("%d",&b);
+   c=a+b;
+   printf("sum of values are : %d",c);
+   getch();
+}
